3c998d161c31cb38c81a5cdbf645250e66ec3586
Diego <fonseca.diego@outlook.com>
2022-09-24 13:45:13 -0400

    background and stuff

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.FlxCamera;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
@@ -11,16 +12,32 @@ import feshixl.FeshCamera;
 import feshixl.FeshSprite;
 
 class PlayState extends BetterUIStates {
+	private var camGame:FeshCamera;
+	private var camGen:FeshCamera;
+
 	override public function create():Void {
+		camGame = new FeshCamera();
+
+		FlxG.cameras.setDefaultDrawTarget(camGame, true);
+		FlxG.cameras.reset(camGame);
+
+		persistentUpdate = true;
+		persistentDraw = true;
+
 		var sky:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.fromRGB(0, 75, 75));
 		sky.scrollFactor.set(0, 0);
 		add(sky);
 
 		super.create();
-		add(new FlxText("Hello World", 32).screenCenter());
 	}
 
 	override public function update(elapsed:Float){
 		super.update(elapsed);
+	}
+
+	function start_HelloWorld():Void {
+		var helloWorld:FlxText = new FlxText("Hello World", 32);
+		helloWorld.screenCenter();
+		add(helloWorld);
 	}
 }
