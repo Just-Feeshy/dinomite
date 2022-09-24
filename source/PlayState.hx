006721e85c39f7e4d80e828b8699364de2e052f2
Diego <fonseca.diego@outlook.com>
2022-09-24 13:27:45 -0400

    assets

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,21 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
+
+import feshixl.FeshCamera;
+import feshixl.FeshSprite;
 
 class PlayState extends BetterUIStates {
 	override public function create():Void {
+		var sky:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.fromRGB(0, 75, 75));
+		sky.scrollFactor.set(0, 0);
+		add(sky);
+
 		super.create();
 		add(new FlxText("Hello World", 32).screenCenter());
 	}
