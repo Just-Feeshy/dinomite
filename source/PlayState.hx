e78118e989a884403f94bb5731e367af986e868b
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-09-26 17:26:56 -0400

    jumping

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -21,6 +21,9 @@ class PlayState extends BetterUIStates {
 	private var terrain:Terrain;
 
 	private var gravity:Float = 100000;
+	private var jumpForce:Float = 0;
+
+	private var addJumpForce:Bool = false;
 
 	override public function create():Void {
 		camGame = new FeshCamera();
@@ -59,17 +62,34 @@ class PlayState extends BetterUIStates {
 		terrain.clearBlocksBeforeX(terrain.collisionMembers, player.x - 64);
 		player.gravity = topCollision(player, elapsed);
 
+		if(controls.UP && (jumpForce > -2000 && player.isTouchingGround)) {
+			jumpForce -= 200;
+			addJumpForce = true;
+		}else if(addJumpForce) {
+			player.jumpForce = jumpForce;
+			addJumpForce = false;
+			jumpForce = 0;
+		}
+
 		super.update(elapsed);
 	}
 
 	function topCollision(p:Player, elapsed:Float):Float {
+		player.isTouchingGround = false;
+
 		if(gravity <= 0) {
 			gravity = 100000;
 		}
 
 		for(i in 0...terrain.collisionMembers.length) {
 			if(Math.floor(terrain.collisionMembers[i].x / 64) * 64 == Math.floor(p.x / 64) * 64 || Math.ceil(terrain.collisionMembers[i].x / 64) * 64 == Math.ceil(p.x / 64) * 64) {
-				if(p.y < terrain.collisionMembers[i].y) {
+				if(p.y >= terrain.collisionMembers[i].y - 64 && p.y < terrain.collisionMembers[i].y) {
+					p.isTouchingGround = true;
+
+					if(p.gravity != 0) {
+						player.jumpForce = 0;
+					}
+
 					gravity = 0;
 					break;
 				}
--- source/Player.hx
+++ source/Player.hx
@@ -7,13 +7,14 @@ class Player extends FeshSprite {
     public var gravity:Float = 0;
     public var jumpForce:Float = 0;
 
+    public var isTouchingGround:Bool = false;
+
     public function new(x:Float = 0, y:Float = 0) {
         super(x, y);
     }
 
     override function update(elapsed:Float):Void {
         y += (jumpForce * elapsed) + ((gravity * Math.pow(elapsed, 2)) * 0.5);
-        jumpForce -= Math.min(jumpForce - (elapsed * 10), 0);
 
         super.update(elapsed);
     }
