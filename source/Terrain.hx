75ca46d9ac6e2f420fcea8936a46b03bc47d23bc
Diego <fonseca.diego@outlook.com>
2022-09-26 00:45:22 -0400

    faster

--- source/Terrain.hx
+++ source/Terrain.hx
@@ -133,7 +133,7 @@ class Terrain extends FlxSpriteGroup {
         super.update(elapsed);
 
         if(!collisionWall) {
-            backwardsVelocity.x += elapsed * 10;
+            backwardsVelocity.x += elapsed * 25;
         }else {
             backwardsVelocity.x = 0;
         }
